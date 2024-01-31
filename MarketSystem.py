413a653f383e926dcbe2d1cef7e78a576404a527
kristi5491 <ksavchin05@gmail.com>
2024-01-31 12:06:47 +0200

    market2

--- MarketSystem.py
+++ MarketSystem.py
@@ -12,32 +12,57 @@ class Bucket:
     def __init__(self,client_fname, client_lname, mony):
         self.bucket_list = []
         self.balance = Balance(client_fname, client_lname, mony)
-    def throw_to_bucket(self, product_name, amount):
+
+    def __overall_sum(self, product):
+        overall_amount = 0
+        for product_in_bucket in self.bucket_list:
+           overall_amount += product_in_bucket.amount
+        return overall_amount
+
+    def throw_to_bucket(self, product_name, amount_product, price_product):
         for product in self.bucket_list:
             if product.product == product_name:
-                if product not in self.bucket_list:
-                    self.bucket_list.append(product)
-                else:
-                    self.amount = amount
-                    amount += 1
+                current_amount = product.amount + amount_product
+                current_price = current_amount * product.price
+                self.bucket_list.append(product)
+                print(f'{product_name} добавлено в корзину та почточна ціна {current_price}')
+                break
+        else:
+            new_product = Product(product_name, amount_product, price_product)
+            self.bucket_list.append(new_product)
+            print(f'Тепер в корзині  {product_name}')
     def return_product(self, product_name,amount_to_delete):
         for product in self.bucket_list:
             if product.product == product_name:
                 if product.amount >= amount_to_delete:
-                    product.amount -= amount_to_delete
-                    print(f'{product_name} успішно видалили з корзини')
+                    current_amount = product.amount - amount_to_delete
+                    current_price = current_amount * product.price
+                    print(f'{product_name}:{current_price} успішно видалили з корзини')
+                    break
                 else:
                     print(f'Недостатньо кількості продукту {product_name} у корзині')
         else:
             print(f'Продукту {product_name} не знайдено в корзині')
     def buy_products(self):
-        if self.balance.mony >= self.overall_amount:
+        overall_sum = 0
+        for product in self.bucket_list:
+            overall_sum += self.__overall_sum(product)
+        if self.balance.mony >= overall_sum:
+            self.balance.mony -= overall_sum
             print(f'Продукти які ви купили:{self.bucket_list}')
             self.bucket_list.clear()
         else:
             print('На вашому балансі не достатньо грошей')
 
+cart = bucket = Bucket("John", "Doe", 100)
 
+bucket.throw_to_bucket("Apple", 5, 2.5)
+bucket.throw_to_bucket("Banana", 3, 1.0)
 
+bucket.throw_to_bucket("Apple", 5, 2.5)
+
+bucket.return_product("Banana", 2)
+
+bucket.buy_products()
 
 
